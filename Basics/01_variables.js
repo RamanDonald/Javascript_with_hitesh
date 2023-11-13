4f2c254e1197deffe93255d2b4a9efbadd3384e2
Raman yadav <40426625+RamanDonald@users.noreply.github.com>
2023-11-13 05:46:48 +0000

    container config done

---
+++ Basics/01_variables.js
@@ -0,0 +1,8 @@
+const RollNum=52
+let Name="Raman Yadav"
+var State ="Bihar"
+country="India"
+console.table([RollNum,Name,State,country])
+/*
+  dont use var variable because of issue in block scope and functional scope
+*/
