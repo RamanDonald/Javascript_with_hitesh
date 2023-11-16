ac70eb9fad4a0633abf61e07355fa2eb85f5893a
Raman yadav <40426625+RamanDonald@users.noreply.github.com>
2023-11-16 13:16:43 +0000

    repository is done

---
+++ Basics/02_string.js
@@ -0,0 +1,7 @@
+const Name="Raman Yadav"
+console.log(Name.split(' ').length);
+console.log(Name.toUpperCase());
+console.log(Name.toLocaleUpperCase());
+const num=new Number(100)
+console.log(num.toString());
+console.log(num.toString().length);
